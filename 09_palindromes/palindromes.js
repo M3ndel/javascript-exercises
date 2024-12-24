3d6cb3e72ba1fef78b3d043786a8e1683546eab5
M3ndel <33386777+M3ndel@users.noreply.github.com>
2024-12-25 00:47:27 +0800

    pass all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = str.toLowerCase()
+	let arr = str.split('')
+	let processed = arr.reduce((a, b) => {
+		if (('A' <= b && b <= 'Z') || 
+			('a' <= b && b <= 'z') || 
+			('0' <= b && b <= '9')
+		) return a+b
+		else return a
+	}, '')
+	for (let i = 0; i < Math.floor(processed.length/2); i++) {
+		if (processed[i] != processed[processed.length - 1 - i]) return false
+	}
+	return true
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	n = +n
+	if (!Number.isInteger(n) || n < 0) return 'OOPS'
 
+	let perv = 0
+	let curr = 1
+	for (let i=0; i < n; i++) {
+		let temp = curr
+		curr = curr + perv
+		perv = temp
+	}
+	return perv
 };
 
 // Do not edit below this line
