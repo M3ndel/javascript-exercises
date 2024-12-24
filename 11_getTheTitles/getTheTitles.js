f7504172b3145e07877f0d3eece1577ab8862752
M3ndel <33386777+M3ndel@users.noreply.github.com>
2024-12-25 01:26:16 +0800

    Passs all test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(lib) {
+	return lib.map((book) => book.title);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ppl) {
+	return ppl.reduce((a, b) => {
+		if (((b.yearOfDeath??= new Date().getFullYear()) - b.yearOfBirth) > (a.yearOfDeath??= new Date().getFullYear()) - a.yearOfBirth) {
+			return b
+		}
+		else return a
+	}, ppl[0])
 };
 
 // Do not edit below this line
